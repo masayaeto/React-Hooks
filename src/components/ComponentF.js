385ee77fcc226ec1e0477020058de25a78380714
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-02-21 10:06:30 +0900

    useContextの取り扱いについて

---
+++ src/components/ComponentF.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+import {UserContext, LanguageContext} from '../App'
+
+function ComponentF() {
+  const user = useContext(UserContext)
+  const language = useContext(LanguageContext)
+  return (
+    <div>
+      <div>{user.name} : {language}</div>
+    </div>
+  )
+}
+
+export default ComponentF
